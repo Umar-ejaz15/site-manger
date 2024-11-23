71c7e4b7c2fb7e0ca2509ebab8a2ec53eae797bd
Umar Ejaz <umar.senpaiiii@gmail.com>
2024-11-23 15:29:26 -0800

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import AllRouter from "./components/AllRouter";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import SiteList from "./pages/SiteList";
 
 function App() {
-  return <AllRouter />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgetpass" element={<ResetPassword />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/addsite" element={<SiteList />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
